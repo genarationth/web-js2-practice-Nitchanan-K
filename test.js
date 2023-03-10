833f66c909dcb64ef094ac079c04ecb0bfcdbb80
Nitchanan - K <108390376+Nitchanan-K@users.noreply.github.com>
2023-03-10 14:47:08 +0000

    first commit

---
+++ test.js
@@ -0,0 +1,24 @@
+// // Challenge 1: Create an Age Calculator
+// Store your birth year in a variable.
+// Store a future year in another variable.
+// Calculate your possible ages for the future year and display it in the console.
+// Example: If you were born in 1988, then in 2026 you’ll be 37 or 38 (depending on the month in 2026).
+
+my_birth_year = 1997;
+
+future_year = 2030;
+
+console.log(future_year - my_birth_year);
+
+
+// Challenge 2: Create an information card
+// Create a card holding 3 pieces of information about a person, such as Name, Location, and Hobby.
+// Each piece of information should be a variable.
+// Display the card with the information in the correct place.
+// Example: “Hi, my name is John. I live in South Africa and enjoy drinking homemade wine.”
+
+let Name = "Tee";
+let location = "Phuket";
+let hobby = "Running";
+
+console.log("Hi, my name is " + Name + ". I live in " + location + " and enjoy " + hobby + ".");
