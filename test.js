636132cbf1aa20be3eff35d216c9c6f79106e286
Nitchanan - K <108390376+Nitchanan-K@users.noreply.github.com>
2023-03-11 06:06:00 +0000

    add card

--- test.js
+++ test.js
@@ -11,14 +11,5 @@ future_year = 2030;
 console.log(future_year - my_birth_year);
 
 
-// Challenge 2: Create an information card
-// Create a card holding 3 pieces of information about a person, such as Name, Location, and Hobby.
-// Each piece of information should be a variable.
-// Display the card with the information in the correct place.
-// Example: “Hi, my name is John. I live in South Africa and enjoy drinking homemade wine.”
 
-let Name = "Tee";
-let location = "Phuket";
-let hobby = "Running";
 
-console.log("Hi, my name is " + Name + ". I live in " + location + " and enjoy " + hobby + ".");
